1457c46c095f33be187bea69100b93e140355e68
oriokalani <orio.kalani@gmail.com>
2023-09-16 22:36:21 -0500

    fixed Deprecation message, completed up to Part 2.2

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,10 +11,21 @@ let candidateAnswer = '';
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
-
+let questions = [
+  '1) Who was the first American woman in space? ',
+  '2) True or false: 5 kilometer == 5000 meters? ',
+  '3) (5 + 3)/2 * 10 = ? ',
+  "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  '5) What is the minimum crew size for the ISS? '
+];
+let correctAnswers = [
+  'Sally Ride',
+  'true',
+  '40',
+  'Trajectory',
+  '3'
+];
+let candidateAnswers = [];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -23,19 +34,21 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question);
-
+  //candidateAnswer = input.question(question);
+  for (let i = 0; i < questions.length; i++) {
+    let userInput = input.question(questions[i]);
+    candidateAnswers.push(userInput);
+  }
+  return candidateAnswers;
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if (candidateAnswer === correctAnswer) {
-    console.log('CORRECT');
-  } else {
-    console.log('INCORRECT');
+  for (let i = 0; i < candidateAnswers.length; i++) {
+    let messageFeedback = `Your Answer: ${candidateAnswers[i]}\nCorrect Answer: ${correctAnswers[i]}`;
+    console.log(messageFeedback);
   }
-
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
@@ -46,7 +59,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log(`Hello, ${candidateName}!`);
+   // console.log(`Hello, ${candidateName}!`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
