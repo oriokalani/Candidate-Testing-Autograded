fe5826dc7560aa9a8c3aed35e1df6c0d8ef121c2
oriokalani <orio.kalani@gmail.com>
2023-09-25 14:16:14 -0500

    test 8 failing despite having trailing white space in questions array

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,11 +12,11 @@ let candidateAnswer = '';
 
 //TODO: Variables for Part 2
 let questions = [
-  '1) Who was the first American woman in space? ',
-  '2) True or false: 5 kilometer == 5000 meters? ',
-  '3) (5 + 3)/2 * 10 = ? ',
+  "1) Who was the first American woman in space? ",
+  "2) True or false: 5 kilometer == 5000 meters? ",
+  "3) (5 + 3)/2 * 10 = ? ",
   "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
-  '5) What is the minimum crew size for the ISS? '
+  "5) What is the minimum crew size for the ISS? "
 ];
 let correctAnswers = [
   'Sally Ride',
@@ -39,7 +39,6 @@ function askQuestion() {
     let userInput = input.question(questions[i]);
     candidateAnswers.push(userInput);
   }
-  return candidateAnswers;
 }
 
 function gradeQuiz(candidateAnswers) {
