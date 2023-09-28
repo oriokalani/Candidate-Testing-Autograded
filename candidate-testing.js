ac4a67e7aae4bb6206d851ce19a85f37075d57e2
oriokalani <orio.kalani@gmail.com>
2023-09-27 21:14:04 -0500

    Assignment 1 COMPLETE, passed test 8, passed tests 1-17, concat userInput, took away numbered list in questions array

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,11 +12,11 @@ let candidateAnswer = '';
 
 //TODO: Variables for Part 2
 let questions = [
-  "1) Who was the first American woman in space?\nYour Answer: ",
-  "2) True or false: 5 kilometer == 5000 meters?\nYour Answer: ",
-  "3) (5 + 3)/2 * 10 = ?\nYour Answer: ",
-  "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?\nYour Answer: ",
-  "5) What is the minimum crew size for the ISS?\nYour Answer: "
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "
 ];
 let correctAnswers = [
   'Sally Ride',
@@ -36,7 +36,7 @@ function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   //candidateAnswer = input.question(question);
   for (let i = 0; i < questions.length; i++) {
-    let userInput = input.question(questions[i]);
+    let userInput = input.question(questions[i] +"\nYour Answer: ");
     candidateAnswers.push(userInput);
     console.log(`Correct Answer: ${correctAnswers[i]}\n`);
   }
